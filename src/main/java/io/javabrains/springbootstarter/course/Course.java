1e5d01609b5fa4ad236566331b2b62aeb4c42e5c
Muhammad Qureshi <muhammad.qureshi@sky.uk>
2018-02-20 09:41:04 +0000

    20022018 - Added Lombok

--- src/main/java/io/javabrains/springbootstarter/course/Course.java
+++ src/main/java/io/javabrains/springbootstarter/course/Course.java
@@ -1,12 +1,14 @@
 package io.javabrains.springbootstarter.course;
 
 import io.javabrains.springbootstarter.topic.Topic;
+import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
+@Data
 public class Course {
 
     @Id
@@ -15,47 +17,4 @@ public class Course {
     private String description;
     @ManyToOne
     private Topic topic;
-
-    public Course() {
-
-    }
-
-    public Course(String id, String name, String description, String topicId) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.topic = new Topic(topicId, "", "");
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Topic getTopic() {
-        return topic;
-    }
-
-    public void setTopic(Topic topic) {
-        this.topic = topic;
-    }
 }
--- src/main/java/io/javabrains/springbootstarter/topic/Topic.java
+++ src/main/java/io/javabrains/springbootstarter/topic/Topic.java
@@ -1,9 +1,12 @@
 package io.javabrains.springbootstarter.topic;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Topic {
 
     @Id
@@ -19,29 +22,5 @@ public class Topic {
         this.id = id;
         this.name = name;
         this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
